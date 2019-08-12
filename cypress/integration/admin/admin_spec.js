ddb270341783d0f5a41ace89e0e50ae9393469f5
Peter Ivanov <peter@microweber.com>
2019-08-12 20:11:12 +0300

    updates

--- cypress/integration/admin/admin_spec.js
+++ cypress/integration/admin/admin_spec.js
@@ -1,66 +1,12 @@
-describe('The Admin Page', function() {
-    it('successfully loads', function() {
-        cy.clearCookies()
+import { fixCypressSpec } from '../../support'
 
-        cy.visit('/admin')
-    })
+beforeEach(fixCypressSpec(__filename))
 
 
-    it('sets auth cookie when logging in via form submission', function () {
-        // destructuring assignment of the this.currentUser object
-
-
-        const username = Cypress.env('username')
-        const  password  = Cypress.env('password')
-
-
-        cy.get('#lang_selector li').its('length').should('be.gt', 2)
-        cy.get('#login_foot a').its('length').should('be.gt', 1)
-
-        cy.get('input[name=username]').type(username)
-
-        // {enter} causes the form to submit
-        cy.get('input[name=password]').type(`${password}{enter}`)
-
-        // we should be redirected to /dashboard
-        cy.url().should('include', '/admin')
-
-        cy.get('#mw-admin-main-menu li').its('length').should('be.gt', 3)
-
-
-        // our auth cookie should be present
-        cy.getCookie('laravel_session',{timeout:3000}).should('exist')
-
-        // UI should reflect this user being logged in
-       // cy.get('h1').should('contain', 'jane.lane')
-    })
-
-
-    it('test admin dashboard', function () {
-
-        cy.get('#site-stats-admin-site-stats-dashboard-graph').should('exist')
-        cy.get('#admin-dashboard-recent-comments').should('exist')
-        cy.get('#create-content-menu').should('exist')
-        //cy.wait(1500).get('body',{timeout:3000})
-        //    .toMatchImageSnapshot({
-        //
-        //    });
+const test1 = require('./describe1');
+test1.initPlugin();
 
 
 
-
-    })
-
-    it('test admin snapshot 1', function () {
-        cy.wait(1500).get('#main-bar',{timeout:3000}).toMatchImageSnapshot();
-    })
-
-    it('test admin snapshot 2', function () {
-        cy.wait(1500).get('#main-bar-mw-icon',{timeout:3000}).toMatchImageSnapshot();
-    })
-    it('test admin snapshot 3', function () {
-        cy.wait(1500).get('.admin-toolbar',{timeout:3000}).toMatchImageSnapshot({
-            clip: { x: 0, y: 0, width: 100, height: 100 },
-        });
-     })
-})
+const test2 = require('./describe2');
+test2.initPlugin();
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -19,5 +19,19 @@ import './commands'
 import 'cypress-plugin-snapshots/commands';
 
 
+// cypress/support/index.js
+export const fixCypressSpec = filename => () => {
+    const path = require('path')
+    const relative = filename.substr(1) // removes leading "/"
+    const projectRoot = Cypress.config('projectRoot')
+    const absolute = path.join(projectRoot, relative)
+    Cypress.spec = {
+        absolute,
+        name: path.basename(filename),
+        relative
+    }
+}
+
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
