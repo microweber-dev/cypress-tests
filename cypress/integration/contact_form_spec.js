1db8e335f929953c8f070d59e600434a0644a361
b.slaveykov <b.slaveykov@radioactive.bg>
2019-09-03 14:15:26 +0300

    update

--- cypress/integration/contact_form_spec.js
+++ cypress/integration/contact_form_spec.js
@@ -3,71 +3,63 @@
 describe('The Contact Page', function() {
 	it('successfully loads', function() {
 		
-		cy.clearCookies()
+		cy.login('admin');
+		cy.moduleContactFormDisableCaptcha();
+		
 		cy.visit('/contacts')
 
-		var Name = 'Bozhidar Slaveykov'
-		var Email = 'bobi@microweber.com'
-		var Message = 'This is my message.'
+		var first_name = 'Bozhidar Slaveykov'
+		var email = 'bobi@microweber.com'
+		var message = 'This is my message.'
+		var company_position = 'Company position'
+		var phone = 'Phone'
+		var company_name = 'Company name'
+		var company_registration = 'Company registration'
+	
+		cy.get('input[name=first_name]').scrollIntoView() 
 
 		cy.server()
 		cy.route('POST', '**/api/post_form').as('postForm')
 
-		cy.get('input[name=Name]').type(Name).should('have.value', Name)
-		cy.get('input[name=Email]').type(Email).should('have.value', Email)
-		cy.get('textarea[name=Message]').type(Message).should('have.value', Message)
+		cy.get('input[name=first_name]').type(first_name).should('have.value', first_name)
+		cy.get('input[name=email]').eq(0).type(email).should('have.value', email)
+		cy.get('textarea[name=message]').type(message).should('have.value', message)
+		cy.get('input[name=company_position]').type(company_position).should('have.value', company_position)
+		cy.get('input[name=company_name]').type(company_name).should('have.value', company_name)
+		cy.get('input[name=company_registration]').type(company_registration).should('have.value', company_registration)
+		cy.get('input[name=phone]').type(phone).should('have.value', phone)
 
-		cy.get('.contact-form').find('.mw_form').submit() 
+		cy.get('input[name=first_name]').parent().parent().parent().find('form').submit() 
 		
-		var entryId = 0
+		var entry_id = 0
 		cy.wait('@postForm').then(function(xhr){
 		  
-		  entryId = xhr.response.body.id
+		  entry_id = xhr.response.body.id
 		  
-		  loginAdmin()
-		  checkEntry(entryId, Name, Email, Message)
+		  cy.login('admin');
+		  checkEntry(entry_id, first_name, email, message)
 		  
 		})
-
-		function loginAdmin() {
-
-			cy.clearCookies()
-			cy.visit('/admin')
-
-
-			const username = Cypress.env('username')
-			const password = Cypress.env('password')
-
-
-			cy.get('#lang_selector li').its('length').should('be.gt', 2)
-			cy.get('#login_foot a').its('length').should('be.gt', 1)
-
-			cy.get('input[name=username]').type(username)
-
-			// {enter} causes the form to submit
-			cy.get('input[name=password]').type(`${password}{enter}`)
-
-			// we should be redirected to /dashboard
-			cy.url().should('include', '/admin')
-
-			cy.get('#mw-admin-main-menu li').its('length').should('be.gt', 3)
-
-			cy.getCookie('laravel_session', {timeout: 3000}).should('exist')
-		}
 		
-		function checkEntry(entryId, Name, Email, Message) {
+		function checkEntry(entry_id, first_name, email, message) {
 
 			cy.get('a').contains('Notifications').click()
 			
 			cy.get('.all-notifications').find('.mw-ui-link').eq(0).click()
 			
-			cy.request('/api/get_contact_entry_by_id?id=' + entryId)
+			cy.request('/api/get_contact_entry_by_id?id=' + entry_id)
 			  .then((response) => {
 				
-				expect(response.body).to.have.property('id', entryId)
-				expect(response.body.custom_fields).to.have.property('Name', Name)
-				expect(response.body.custom_fields).to.have.property('Email', Email)
-				expect(response.body.custom_fields).to.have.property('Message', Message)
+				expect(response.body).to.have.property('id', entry_id)
+				expect(response.body.custom_fields).to.have.property('first_name', first_name)
+				expect(response.body.custom_fields).to.have.property('email', email)
+				expect(response.body.custom_fields).to.have.property('message', message)
+				
+				expect(response.body.custom_fields).to.have.property('phone', phone)
+				expect(response.body.custom_fields).to.have.property('company_position', company_position)
+				expect(response.body.custom_fields).to.have.property('company_name', company_name)
+				expect(response.body.custom_fields).to.have.property('company_registration', company_registration)
+				
 				
 			  })
 		}
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,141 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('login', (userType, options = {}) => {
+	
+	cy.clearCookies()
+	cy.visit('/admin')
+
+	const username = Cypress.env('username')
+	const password = Cypress.env('password')
+
+
+	cy.get('#lang_selector li').its('length').should('be.gt', 2)
+	cy.get('#login_foot a').its('length').should('be.gt', 1)
+
+	cy.get('input[name=username]').type(username)
+
+	// {enter} causes the form to submit
+	cy.get('input[name=password]').type(`${password}{enter}`)
+
+	// we should be redirected to /dashboard
+	cy.url().should('include', '/admin')
+
+	cy.get('#mw-admin-main-menu li').its('length').should('be.gt', 3)
+
+	cy.getCookie('laravel_session', {timeout: 3000}).should('exist')
+	
+});
+
+
+Cypress.Commands.add('moduleContactFormDisableCaptcha', () => {
+	
+	cy.wait(1500)  
+	
+	cy.get('.mw-admin-go-live-now-btn').invoke('removeAttr', 'target').click();
+	
+	cy.visit('/contacts') 
+	
+	cy.get('div[data-type=contact_form]').scrollIntoView({ duration: 1000 , offset: {top: -400}}) 
+
+	// cy.get('div[data-type=contact_form]').find('h4').scrollIntoView({ duration: 1000 , offset: {top: -300}}).click()
+	
+	cy.window().then((win) => {
+	
+		
+	cy.get('div[data-type=contact_form]')
+     .should('have.attr', 'id')
+     .then((id) => { 
+		
+		var modal_contact_form = win.mw.tools.open_global_module_settings_modal('contact_form/admin', id)
+		
+		 cy.wait(1500)
+		 .get('.mw-dialog-container')
+		 .find('iframe').first().iframe()
+		 .find('input[name=disable_captcha]').click({force:true}).then(() => {
+			modal_contact_form.remove()
+			// cy.visit('/contacts')
+		 });
+		
+     })
+	
+
+	});
+});
+
+Cypress.Commands.add("iframe", { prevSubject: "element" }, $iframe => {
+  Cypress.log({
+    name: "iframe",
+    consoleProps() {
+      return {
+        iframe: $iframe,
+      };
+    },
+  });
+
+  return new Cypress.Promise(resolve => {
+    onIframeReady(
+      $iframe,
+      () => {
+        resolve($iframe.contents().find("body"));
+      },
+      () => {
+        $iframe.on("load", () => {
+          resolve($iframe.contents().find("body"));
+        });
+      }
+    );
+  });
+});
+
+function onIframeReady($iframe, successFn, errorFn) {
+  try {
+    const iCon = $iframe.first()[0].contentWindow,
+      bl = "about:blank",
+      compl = "complete";
+    const callCallback = () => {
+      try {
+        const $con = $iframe.contents();
+        if ($con.length === 0) {
+          // https://git.io/vV8yU
+          throw new Error("iframe inaccessible");
+        }
+        successFn($con);
+      } catch (e) {
+        // accessing contents failed
+        errorFn();
+      }
+    };
+
+    const observeOnload = () => {
+      $iframe.on("load.jqueryMark", () => {
+        try {
+          const src = $iframe.attr("src").trim(),
+            href = iCon.location.href;
+          if (href !== bl || src === bl || src === "") {
+            $iframe.off("load.jqueryMark");
+            callCallback();
+          }
+        } catch (e) {
+          errorFn();
+        }
+      });
+    };
+    if (iCon.document.readyState === compl) {
+      const src = $iframe.attr("src").trim(),
+        href = iCon.location.href;
+      if (href === bl && src !== bl && src !== "") {
+        observeOnload();
+      } else {
+        callCallback();
+      }
+    } else {
+      observeOnload();
+    }
+  } catch (e) {
+    // accessing contentWindow failed
+    errorFn();
+  }
+}
