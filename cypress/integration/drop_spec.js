e5ef5ee880af2f38abf80b63b3a4377b87cf872a
alex <raikov@ymail.com>
2019-09-03 21:47:35 +0300

    drag test

--- cypress/integration/drop_spec.js
+++ cypress/integration/drop_spec.js
@@ -1,16 +1,36 @@
 /// <reference types="Cypress" />
 
-describe('Page test', function() {
-	it('successfully loads', function() {
-		
-		cy.login('admin');
-		cy.wait(1500);
-		cy.visit('http://127.0.0.6/microweber/home?editmode=y')
-		//cy.get('#mw-modules-layouts-tabsnav').find('a').first().click({force:true})
+describe('Page test', () => {
+    it('successfully loads', () => {
+
+        cy.login('admin');
+        cy.wait(1500);
+        cy.visit('http://localhost/mw1/?editmode=y')
+        cy.wait(1500);
+        cy.get('.mw-lslayout-tab', {timeout: 1000}).click({force:true});
+        cy.wait(1500);
+        cy.get('#mw-sidebar-modules-and-layouts-holder', {timeout: 3000}).find('li').first().as('firstLayout');
+    cy.get('body').as('body')
+        cy.wait(1500);
+        cy.get('@firstLayout')
+            .trigger('mousedown', { which: 1})
+
+    
+        cy.get('@body')
+            .trigger('mousemove', { pageX: 200, pageY: 55, which: 1 })
+            .trigger('mouseup');
 
 
-	
-	})
+
+        cy.get('body')
+            .trigger('mousemove', { pageX: 5, pageY: 137 })
+            .trigger('mousedown', { which: 1})
+            .trigger('mousemove', { pageX: 5, pageY: 560 })
+            .trigger('mouseup');
+
+
+
+    })
 })
 
 
